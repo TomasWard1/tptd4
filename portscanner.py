a6435a8f78677682075fe58d5688b974e0833bc5
Tomas Ward <tomas@familiaward.com.ar>
2023-10-20 12:25:32 -0300

    porstcanner 2.2

--- portscanner.py
+++ portscanner.py
@@ -2,23 +2,36 @@ from scapy.layers.inet import IP, ICMP, sr1, TCP
 import sys
 
 
-def port_scanner(ip_address):
+def port_scanner(ip_address,mode):
     open_ports = []
     filtered_ports = []
-    total_ports = 1000  # You can adjust this based on your needs
-    output_file = "port_scan_results.txt"
+    total_ports = 1000
+    output_file = "port_scan_results_-f.txt"
 
     for dest_port in range(1, total_ports + 1):
-        packet = IP(dst=ip_address) / TCP(flags="S", dport=dest_port)
-        resp = sr1(packet, timeout=1, verbose=1)
+        try:
+            packet = IP(dst=ip_address) / TCP(flags="S", dport=dest_port)
+            resp = sr1(packet, timeout=0.5, verbose=1)
 
-        # 0x12 == SYN-ACK
-        if resp is not None and resp.haslayer(TCP) and resp.getlayer(TCP).flags == 0x12:
-            # Puerto esta abierto
-            open_ports.append(dest_port)
-        elif resp is None:
-            # No se recibio respuesta, esta filtrado
-            filtered_ports.append(dest_port)
+            if resp is not None and resp.haslayer(TCP) and resp.getlayer(TCP).flags == 'SA':
+                if (mode == '-h'):
+                    #Criterio 2.1: Puerto esta abierto
+                    open_ports.append(dest_port)
+
+                elif (mode == '-f'):
+                    #Criterio 2.2: Hay que establecer conexion TCP para confirmar estado abierto
+                    #Mandamos un paquete con ACK + Payload
+                    packet_with_payload = IP(dst=ip_address) / TCP(flags="A", ack=resp.getlayer(TCP).seq + 1, dport=dest_port) / "Payload"
+                    resp_with_payload = sr1(packet_with_payload, timeout=1, verbose=0)
+                    if resp_with_payload is not None and resp_with_payload.haslayer(TCP) and resp_with_payload.getlayer(TCP).flags & 0x10:
+                         open_ports.append(dest_port)
+            
+            elif resp is None:
+                # No se recibio respuesta, esta filtrado
+                filtered_ports.append(dest_port)
+        except ConnectionRefusedError as e:
+            #Puerto cerrado
+            print(f"Connection refused: {e}")
 
     with open(output_file, "w") as f:
         f.write("Open Ports:\n")
@@ -37,9 +50,11 @@ def port_scanner(ip_address):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python portscanner.py <IP>")
+    if len(sys.argv) != 3:
+        print("Uso: python portscanner.py <IP> <-h/-f>")
         sys.exit(1)
 
     target_ip = sys.argv[1]
-    port_scanner(target_ip)
+    mode_char = sys.argv[2]
+
+    port_scanner(target_ip,mode_char)
