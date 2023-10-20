2e5db6972952c0ed7cd1b0a08593036b27c9af92
Tomas Ward <tomas@familiaward.com.ar>
2023-10-20 11:24:17 -0300

    sdsd

--- main.py
+++
@@ -1,4 +0,0 @@
-from scapy.all import *
-direccionIpDst = "www.utdt.com"
-packet = IP(dst= "www.utdt.com", ttl = 64)/ICMP(type=8, code=0)
-resp = sr1(packet, timeout = 10)
--- traceroute.py
+++ traceroute.py
@@ -1,4 +1,4 @@
-from scapy.layers.inet import IP,ICMP,sr1,sr
+from scapy.layers.inet import IP,ICMP,sr1
 import sys
 import socket
 
@@ -7,29 +7,24 @@ def traceroute(host:str):
     Recibe: IP del host destino
     '''
     i:int = 1
+    destination_ip = socket.gethostbyname(host)
     
     while(i < 64): 
         packet = IP(dst= host, ttl = i)/ICMP(type=8, code=0)
-        resp = sr1(packet, timeout = 10,verbose=0)
+        resp = sr1(packet, timeout = 2,verbose=0)
         if resp is not None:
             response_ip = resp.getlayer(IP).src
            
-            if (response_ip == socket.gethostbyname(host)):
+            if (response_ip == destination_ip):
                 print('The host\'s IP is {}'.format(response_ip))
                 return
             else:
                 print('Hop {}: {}'.format(i,response_ip))
         else:
             pass
-        
         i+=1
     print(i)
     print('traceroute finished')
-      
-      
-
-       
-
 
 def main():
   # Obtenemos el host destino del argumento de la línea de comandos.
