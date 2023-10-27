ddf4c5ffdfb6121bce26df4ab051a1169dfa5f9a
MarianoSanson <111747953+MarianoSanson@users.noreply.github.com>
2023-10-27 15:45:38 -0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -2,52 +2,37 @@ from scapy.layers.inet import IP, ICMP, sr1, TCP
 import sys
 
 
-def port_scanner(ip_address, mode):
+def port_scanner(ip_address,mode):
     open_ports = []
     filtered_ports = []
     total_ports = 1000
-    output_file = "results_f.txt"
+    output_file = "port_scan_results_-f.txt"
 
     for dest_port in range(1, total_ports + 1):
         try:
             packet = IP(dst=ip_address) / TCP(flags="S", dport=dest_port)
-            resp = sr1(packet, timeout=0.5, verbose=0)
+            resp = sr1(packet, timeout = 0.5 , verbose=1)
 
-            if resp is not None and resp.haslayer(TCP) and resp['TCP'].flags == 'SA':
+            if resp is not None and resp.haslayer(TCP) and resp.getlayer(TCP).flags == 'SA':
                 if (mode == '-h'):
-                    # Criterio 2.1: Puerto esta abierto
+                    #Criterio 2.1: Puerto esta abierto
                     open_ports.append(dest_port)
 
                 elif (mode == '-f'):
-                    # Criterio 2.2: Hay que establecer conexion TCP para confirmar estado abierto
-                    # Mandamos un paquete con ACK + Payload
-                    packet_with_payload = IP(
-                        dst=ip_address) / TCP(flags="A", ack=resp.getlayer(TCP).seq + 1, dport=dest_port) / "Payload"
-                    resp_with_payload = sr1(
-                        packet_with_payload, timeout=1, verbose=0)
-                    print(resp_with_payload)
-                    if resp_with_payload is not None and resp_with_payload.haslayer(TCP) and resp_with_payload.getlayer(TCP).flags == 'A':
-                        print('Analyzing port {}, status OPEN'.format(dest_port))
-                        open_ports.append(dest_port)
-
-                    elif resp_with_payload is None:
-                        # No se recibio respuesta, esta filtrado
-                        print('Analyzing port {}, status FILTERED'.format(dest_port))
-                        filtered_ports.append(dest_port)
-                    else:
-                        print('Analyzing port {}, status OTHER'.format(dest_port))
-
+                    #Criterio 2.2: Hay que establecer conexion TCP para confirmar estado abierto
+                    #Mandamos un paquete con ACK + Payload
+                    packet_with_payload = IP(dst=ip_address) / TCP(flags="A", ack=resp.getlayer(TCP).seq + 1, dport=dest_port) / "Payload"
+                    resp_with_payload = sr1(packet_with_payload, timeout=1, verbose=0)
+                    if resp_with_payload is not None and resp_with_payload.haslayer(TCP) and resp_with_payload.getlayer(TCP).flags & 0x10:
+                         open_ports.append(dest_port)
+            
             elif resp is None:
                 # No se recibio respuesta, esta filtrado
-                print('Analyzing port {}, status FILTERED'.format(dest_port))
                 filtered_ports.append(dest_port)
-            else:
-                print('Analyzing port {}, status OTHER'.format(dest_port))
         except ConnectionRefusedError as e:
-            # Puerto cerrado
+            #Puerto cerrado
             print(f"Connection refused: {e}")
 
-    # Procesamos al txt
     with open(output_file, "w") as f:
         f.write("Open Ports:\n")
         for port in open_ports:
@@ -72,4 +57,4 @@ if __name__ == "__main__":
     target_ip = sys.argv[1]
     mode_char = sys.argv[2]
 
-    port_scanner(target_ip, mode_char)
+    port_scanner(target_ip,mode_char)
